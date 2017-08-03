36777447313c9f51c3eded58a7abe69ecf54c7c7
DevartVasiliyK <DevartVasiliyK@devart>
2017-08-03 14:27:55 +0300

    1

---
+++ Tables/dbo.t_2.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[t_2] (
+  [pk] [int] NULL,
+  [fk] [int] NULL
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[t_2]
+  ADD FOREIGN KEY ([fk]) REFERENCES [dbo].[t_3] ([pk])
+GO
---
+++ Tables/dbo.t_3.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[t_3] (
+  [pk] [int] NOT NULL,
+  PRIMARY KEY CLUSTERED ([pk])
+)
+ON [PRIMARY]
+GO
